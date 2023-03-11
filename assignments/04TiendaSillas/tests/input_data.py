f4b23c3a9a2bb9888eeec185815bf3d51d9cca46
roque63 <roque@itesm.mx>
2023-03-10 23:47:23 -0600

    input_data

--- assignments/04TiendaSillas/tests/input_data.py
+++ assignments/04TiendaSillas/tests/input_data.py
@@ -9,176 +9,177 @@ input_values = [
     # Test case 1
     (
         ["1", "5"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","",
          "area = 25.0"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        1. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     (
         ["1", "15.5"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","",
          "area = 240.25"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        2. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     
      (
         ["2", "99.9", "33.9"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","","",
          "area = 3386.61"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        3. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     
     (
         ["2", "10", "20"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","","",
          "area = 200.0"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        4. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     
     (
         ["3", "3", "25","26"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","","","",
          "area = 36.0"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        5. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
      (
         ["3", "3", "25","26"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","","","",
          "area = 36.0"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        6. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     
     (
-        ["3", "10", "12","15"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["3", "10.0", "12.0","15.0"],
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","","","",
          "area = 59.81168364124187"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        7. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
      (
-        ["3", "10", "20","25"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["3", "10.0", "20.0","25.0"],
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ","","","",
          "area = 94.99177595981665"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        8. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     
 (
         ["4"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ",
          "Adios"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        9. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     (
         ["0"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ",
          "Opcion incorrecta"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        10. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
      (
         ["-1"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ",
          "Opcion incorrecta"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        11. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     ),
     (
         ["100"],
-        ["""Area del
-1. Cuadrado
-2. Rectangulo
-3. Triangulo
-4. Salir
-Teclea la opcion: """,
+        ["Area del",
+"1. Cuadrado",
+"2. Rectangulo",
+"3. Triangulo",
+"4. Salir",
+"Teclea la opcion: ",
          "Opcion incorrecta"],
         '''
-        revisa que si leiste los valores como flotantes, 
+        12. revisa que si leiste los valores como flotantes, 
         revisa si la funcion regresa valor
         '''
     )
     
 ]
+
